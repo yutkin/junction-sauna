79f6446609f9c43164d97e3288d7c6a61d0149e6
Misha Khokhlov <hazolsky@gmail.com>
2018-11-24 14:48:26 +0200

    Transfer to RN

---
+++ src/SaunaPlusRN/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+// import { AppLoading, Asset, Font, Icon } from 'expo';
+import AppNavigator from './navigation/AppNavigator';
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="light-content" />}
+        <AppNavigator />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
